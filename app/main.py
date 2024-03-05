63c37b20eb63ed331bb431cafc9af64cee570e11
Claudio Pontes <claudiohpontes@gmail.com>
2024-03-05 00:24:38 -0300

    Adicionado schema de estabelecimentos

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def ler_arquivos_polars(final_arquivo, diretorio, headers) -> pl.DataFrame:
     Args:
         final_arquivo (str): O final do nome do arquivo que se deseja ler.
         diretorio (str): O diretório onde os arquivos estão localizados.
-        header (str): os cabeçalhos que o df deve ter
+        headers (str): os cabeçalhos que o DataFrame Polars deve ter
     Returns:
         DataFrame Polars: Um DataFrame contendo todos os arquivos com o final especificado.
     """
@@ -23,9 +23,7 @@ def ler_arquivos_polars(final_arquivo, diretorio, headers) -> pl.DataFrame:
 
             caminho_arquivo = os.path.join(diretorio, arquivo)
 
-            #df = pl.read_csv(caminho_arquivo, encoding='latin1', has_header=False, separator=';')
-
-            df = pl.read_csv(caminho_arquivo, encoding='latin1', separator=';', new_columns=headers)
+            df = pl.read_csv(caminho_arquivo, encoding='latin1', separator=';', new_columns=headers, ignore_errors=True)
 
             dataframes.append(df)
  
@@ -37,12 +35,21 @@ def ler_arquivos_polars(final_arquivo, diretorio, headers) -> pl.DataFrame:
 
 schema_empresa = schemas.schema_empresa
 final_empresas = '.EMPRECSV'
-headers = list(schema_empresa.columns.keys())
-print(headers)
-df_empresas = ler_arquivos_polars(final_empresas, diretorio, headers)
+headers_empresa = list(schema_empresa.columns.keys())
+
+df_empresas= ler_arquivos_polars(final_empresas, diretorio, headers_empresa)
+df_empresas = df_empresas.with_columns(pl.col('CAPITAL SOCIAL DA EMPRESA').str.replace(',','.'))
+df_empresas = df_empresas.with_columns(pl.col('CAPITAL SOCIAL DA EMPRESA').cast(pl.Float64))
 
 
+schema_estabelecimento = schemas.schema_estabelecimento
+final_estabelecimento = 'ESTABELE'
+headers_estabelecimento = list(schema_empresa.columns.keys())
+
+
+df_estabelecimentos = ler_arquivos_polars(final_estabelecimento, diretorio, headers_estabelecimento)
 
 # Exibe as informações do DataFrame final
 print("DataFrame Final:")
-print(df_empresas)
+print(df_estabelecimentos)
+#print(df_empresas)
--- app/schemas.py
+++ app/schemas.py
@@ -1,11 +1,46 @@
 import pandera as pa
 
 schema_empresa = pa.DataFrameSchema({
-    "CNPJ BÁSICO": pa.Column(pa.String),
+    "CNPJ BÁSICO": pa.Column(pa.Int),
     "RAZÃO SOCIAL / NOME EMPRESARIAL": pa.Column(pa.String),
-    "NATUREZA JURÍDICA": pa.Column(pa.String),
-    "QUALIFICAÇÃO DO RESPONSÁVEL": pa.Column(pa.String),
+    "NATUREZA JURÍDICA": pa.Column(pa.Int),
+    "QUALIFICAÇÃO DO RESPONSÁVEL": pa.Column(pa.Int),
     "CAPITAL SOCIAL DA EMPRESA": pa.Column(pa.Float),
-    "PORTE DA EMPRESA": pa.Column(pa.String),
+    "PORTE DA EMPRESA": pa.Column(pa.Int),
     "ENTE FEDERATIVO RESPONSÁVEL": pa.Column(pa.String)
+})
+
+
+
+schema_estabelecimento = pa.DataFrameSchema({
+    "CNPJ BÁSICO": pa.Column(pa.String, nullable=False),
+    "CNPJ ORDEM": pa.Column(pa.String, nullable=False),
+    "CNPJ DV": pa.Column(pa.String, nullable=False),
+    "IDENTIFICADOR MATRIZ/FILIAL": pa.Column(pa.Int, pa.Check.isin([1, 2]), nullable=False),
+    "NOME FANTASIA": pa.Column(pa.String),
+    "SITUAÇÃO CADASTRAL": pa.Column(pa.String, nullable=False, checks=pa.Check.isin(["01", "02", "03", "04", "08"])),
+    "DATA SITUAÇÃO CADASTRAL": pa.Column(pa.DateTime),
+    "MOTIVO SITUAÇÃO CADASTRAL": pa.Column(pa.String),
+    "NOME DA CIDADE NO EXTERIOR": pa.Column(pa.String),
+    "PAIS": pa.Column(pa.String),
+    "DATA DE INÍCIO ATIVIDADE": pa.Column(pa.DateTime),
+    "CNAE FISCAL PRINCIPAL": pa.Column(pa.String),
+    "CNAE FISCAL SECUNDÁRIA": pa.Column(pa.String),
+    "TIPO DE LOGRADOURO": pa.Column(pa.String),
+    "LOGRADOURO": pa.Column(pa.String),
+    "NÚMERO": pa.Column(pa.String),
+    "COMPLEMENTO": pa.Column(pa.String),
+    "BAIRRO": pa.Column(pa.String),
+    "CEP": pa.Column(pa.String),
+    "UF": pa.Column(pa.String, pa.Check.str_length(2, 2), nullable=False),
+    "MUNICÍPIO": pa.Column(pa.String),
+    "DDD 1": pa.Column(pa.String),
+    "TELEFONE 1": pa.Column(pa.String),
+    "DDD 2": pa.Column(pa.String),
+    "TELEFONE 2": pa.Column(pa.String),
+    "DDD DO FAX": pa.Column(pa.String),
+    "FAX": pa.Column(pa.String),
+    "CORREIO ELETRÔNICO": pa.Column(pa.String),
+    "SITUAÇÃO ESPECIAL": pa.Column(pa.String),
+    "DATA DA SITUAÇÃO ESPECIAL": pa.Column(pa.DateTime)
 })
